18f9cb107d831ecd7b01c378d5cd409315e89792
Mikhail Grigorev <sleuthhound@gmail.com>
2021-04-26 23:13:25 +0500

    Updated release date and version [4.4.19]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "29 March 2021";
+	static final String REVISION_DATE = "26 April 2021";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "4.4.19rc1";
+	static final String VERSION = "4.4.19";
 
 	static void printVersion()
 	{
