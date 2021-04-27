35b8b2f5356105107cc78d0b41a70c47a9e70eeb
Mikhail Grigorev <sleuthhound@gmail.com>
2021-04-27 13:58:42 +0500

    Updated release version [4.4.20rc1]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "26 April 2021";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "4.4.19";
+	static final String VERSION = "4.4.20rc1";
 
 	static void printVersion()
 	{
