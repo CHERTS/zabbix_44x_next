b264c4be8bb2969963f6e6be3e0400d0a7790dc7
Mikhail Grigorev <sleuthhound@gmail.com>
2021-11-30 11:45:38 +0500

    [ZBX-19872] removed redundant escaping in XML data of HTTP agent POST request (asestakovs)

--- include/common.h
+++ include/common.h
@@ -1358,7 +1358,6 @@ int	zbx_strcmp_natural(const char *s1, const char *s2);
 /* additional token flags */
 #define ZBX_TOKEN_NUMERIC	0x008000
 #define ZBX_TOKEN_JSON		0x010000
-#define ZBX_TOKEN_XML		0x020000
 #define ZBX_TOKEN_REGEXP	0x040000
 #define ZBX_TOKEN_XPATH		0x080000
 #define ZBX_TOKEN_REGEXP_OUTPUT	0x100000
